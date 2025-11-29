a163966de6a3c2cd7338475d2122d2561548ccdd
Doug Horner <horner@mieweb.com>
2025-11-29 09:51:11 -0500

    feat: add HTTPS support in Vite configuration for both http and https

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,29 @@
 import { defineConfig } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
-export default defineConfig({
-  plugins: [basicSsl()],
-  server: {
-    cors: true, // Enable CORS for cross-origin requests from production
-    host: 'localhost',
-    port: 5173
-  },
-  build: {
-    outDir: 'dist',
-    lib: {
-      entry: 'src/ychartEditor.ts',
-      name: 'YChartEditor',
-      fileName: () => 'ychart-editor.js',
-      formats: ['iife']
-    },
-    minify: false,
-    target: 'es2015'
+export default defineConfig(({ mode }) => {
+  const plugins = [];
+  if (mode === 'https') {
+    plugins.push(basicSsl());
   }
+
+  return {
+    plugins,
+    server: {
+      cors: true, // Enable CORS for cross-origin requests from production
+      host: 'localhost',
+      port: 5173
+    },
+    build: {
+      outDir: 'dist',
+      lib: {
+        entry: 'src/ychartEditor.ts',
+        name: 'YChartEditor',
+        fileName: () => 'ychart-editor.js',
+        formats: ['iife']
+      },
+      minify: false,
+      target: 'es2015'
+    }
+  };
 });
